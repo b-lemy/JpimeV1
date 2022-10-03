6183b52050ed826c6723ae8e6d2c9510f2202488
b-lema <lema@adanianlabs.io>
2022-10-03 15:43:07 +0300

    editFrontRegisterPage

--- frontend/src/Components/Authentication/Login.js
+++ frontend/src/Components/Authentication/Login.js
@@ -1,12 +1,42 @@
-import React from "react";
-import {Form} from "react-bootstrap";
+import React, {useEffect, useState} from "react";
+import "./Auth.css"
 
 const Login = () => {
+    const [search, setSearch] = useState("");
+
+
+
+    useEffect(() => {
+
+    },[])
+
+
+    const onChanger = (e) => {
+        setSearch(e.target.value)
+
+    }
+
+
+    const Submit = (e) => {
+        e.preventDefault()
+    }
     return(
-        <div>
-            <Form>
-                <label>FirstName</label>
-            </Form>
+        <div className="body">
+            <form onSubmit={Submit} className="container">
+                <h1 className="title"> Login</h1>
+                <label>EmailAddress</label>
+                <input
+                    placeholder="Email"
+                    value={search}
+                    onChange={onChanger}
+
+                />
+                <label>Password</label>
+                <input
+                    placeholder="password"/>
+
+                <button  type="submit">Submit</button>
+            </form>
 
         </div>
     )
