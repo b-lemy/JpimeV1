404d7d4f4ccc0280e94c76f0b215351f7b9438ed
b-lema <lema@adanianlabs.io>
2022-10-26 15:42:34 +0300

    lazy loading

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,29 @@
 import './App.css';
 import {Routes, Route, Link} from "react-router-dom";
-import Login from "./Components/Authentication/Login";
+
 import Register from "./Components/Authentication/Register";
 import Forum from "./Components/Forum/Forum";
 import About from "./Components/Layout/About";
+import {lazy, Suspense} from "react";
+
+//Lazy Loading implementation
+const Login = lazy(() => import("./Components/Authentication/Login"))
 
 function App() {
 
     return (
 
         <div className="App">
-            <Routes>
-                <Route path="/about" element={<About/>}/>
-                <Route path="/login" element={<Login/>}/>
-                <Route path="/" element={<Forum/>}/>
-                <Route path="/register" element={<Register/>}/>
-            </Routes>
+            <Suspense fallback={
+                <div> Loading component</div>
+            }>
+                <Routes>
+                    <Route path="/about" element={<About/>}/>
+                    <Route path="/login" element={<Login/>}/>
+                    <Route path="/" element={<Forum/>}/>
+                    <Route path="/register" element={<Register/>}/>
+                </Routes>
+            </Suspense>
         </div>
     );
 }
