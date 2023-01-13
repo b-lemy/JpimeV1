4f6e689a6fae17804718f158deff9ce342967677
b-lemy <lemabrian1234@gmail.com>
2023-01-13 17:43:42 +0300

    forum design

--- backend/database/factories/UserFactory.php
+++ backend/database/factories/UserFactory.php
@@ -5,16 +5,10 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
- */
+
 class UserFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
+
     public function definition()
     {
         return [
@@ -26,11 +20,7 @@ class UserFactory extends Factory
         ];
     }
 
-    /**
-     * Indicate that the model's email address should be unverified.
-     *
-     * @return static
-     */
+
     public function unverified()
     {
         return $this->state(fn (array $attributes) => [
--- frontend/src/Components/Forum/Forum.js
+++ frontend/src/Components/Forum/Forum.js
@@ -7,56 +7,97 @@ import './Forum.css'
 const Forum = () => {
     const postCtx = useContext(PostContext);
 
-    const [title ,setTitle] = useState('')
-    const [body ,setBody] = useState('')
+    const [title, setTitle] = useState('')
+    const [body, setBody] = useState('')
 
     // const posttitle = useRef();
     // const postbody = useRef();
 
 
-    const addPost = (e)=>{
+    const addPost = (e) => {
         e.preventDefault()
         postCtx.addPost({
-            title:title,
-            body:body
+                title: title,
+                body: body
             }
-
         )
     }
     return (
         <Wrapper>
-            <div className="section">
-                <form onSubmit={addPost}>
-                    <label>Title</label>
-                    <input
-                        className="input"
-                        // ref={posttitle}
-                        value={title}
+            <section className="row section">
+                <div className=" col-lg-8 col-md-8  ">
+                    <div className="start_left">
+                        Posts
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                        Libero id faucibus nisl tincidunt eget nullam. Magna etiam tempor orci eu lobortis.
+                        Consectetur purus ut faucibus pulvinar elementum integer enim neque volutpat.
+                        Mi ipsum faucibus vitae aliquet nec ullamcorper.
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                        Libero id faucibus nisl tincidunt eget nullam. Magna etiam tempor orci eu lobortis.
+                        Consectetur purus ut faucibus pulvinar elementum integer enim neque volutpat.
+                        Mi ipsum faucibus vitae aliquet nec ullamcorper.
+                </div>
+                <div className="start_left">
+                    Posts
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    Libero id faucibus nisl tincidunt eget nullam. Magna etiam tempor orci eu lobortis.
+                    Consectetur purus ut faucibus pulvinar elementum integer enim neque volutpat.
+                    Mi ipsum faucibus vitae aliquet nec ullamcorper.
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    Libero id faucibus nisl tincidunt eget nullam. Magna etiam tempor orci eu lobortis.
+                    Consectetur purus ut faucibus pulvinar elementum integer enim neque volutpat.
+                    Mi ipsum faucibus vitae aliquet nec ullamcorper.
+                </div>
+                </div>
+                <div className=" col-lg-4 col-md-4 ">
+                    <div className="start_right">
+                        Tags
 
-                        onChange={(e)=>setTitle(e.target.value)}
-
-                    />
-
-                    <label>Description</label>
-                    <input
-                        className="input"
-                        // ref={postbody}
-                        value={body}
-                        onChange={(e)=>setBody(e.target.value)}
-
-                    />
-                    <button type="submit">Submit</button>
-                </form>
-
-                <div>
-                    {postCtx.posts.map((post)=>(
-                        <li>{post.title}</li>
-                    ))}
+                    </div>
                 </div>
 
-            </div>
+            </section>
         </Wrapper>
     )
 }
 
 export default Forum;
+
+// <div className="section">
+//     <form onSubmit={addPost}>
+//         <label>Title</label>
+//         <input
+//             className="input"
+//             // ref={posttitle}
+//             value={title}
+//
+//             onChange={(e)=>setTitle(e.target.value)}
+//
+//         />
+//
+//         <label>Description</label>
+//         <input
+//             className="input"
+//             // ref={postbody}
+//             value={body}
+//             onChange={(e)=>setBody(e.target.value)}
+//
+//         />
+//         <button type="submit">Submit</button>
+//     </form>
+//
+//     <div>
+//         {postCtx.posts.map((post)=>(
+//             <ul>
+//                 {post.title}<br/>
+//                 {post.body}
+//             </ul>
+//
+//         ))}
+//     </div>
+//
+// </div>
