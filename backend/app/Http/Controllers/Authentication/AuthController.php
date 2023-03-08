5215fced5568ae746fecace4b67ca4f01ad83be6
b-lemy <lemabrian1234@gmail.com>
2023-03-08 11:20:35 +0300

    quiz app

--- backend/app/Http/Controllers/Authentication/AuthController.php
+++ backend/app/Http/Controllers/Authentication/AuthController.php
@@ -22,7 +22,7 @@ class AuthController extends Controller
     public function login(Request $request)
     {
         if (Auth::attempt($request->only('email', 'password'))) {
-           $user = Auth::User();
+            $user = Auth::User();
             $AToken = $user->createToken('token')->accessToken;
 //            $expiresAt = now()->addDays(7); // set expiration time to 7 days from now
 //            $token = $request->user()->token();
@@ -47,41 +47,32 @@ class AuthController extends Controller
             'password' => Hash::make($request->input('password')),
         ]);
         return response($user, Response::HTTP_CREATED);
-
-
     }
-
 
     public function forgot(ForgotRequest $request)
     {
         $email = $request->input('email');
-        $token = Str::random('12');
-
+        $token = Str::random('1234');
         if (
             User::where('email', $email)->doesntExist()) {
             return response(['message' => 'User doesnt exists'], Response::HTTP_NOT_FOUND);
         }
-
         DB::table('password_resets')->insert(
             [
                 'email' => $email,
                 'token' => $token
             ]
         );
-
         Mail::send("home", ['token' => $token], function (Message $message) use ($email) {
             $message->subject('reset password');
             $message->to($email);
 
         });
-
         return response([
             'message' => 'check your email'
         ]);
 
-
     }
-
     public function reset(ResetRequest $request)
     {
         $passwordReset = DB::table('password_resets')->where('token', $request->input('token'))->first();
--- frontend/src/Components/Quiz/Quiz.js
+++ frontend/src/Components/Quiz/Quiz.js
@@ -1,10 +1,28 @@
 import React from "react";
 import Wrapper from "../Layout/Wrapper";
+import './Quiz.css'
 
 const Quiz = () => {
     return(
         <Wrapper>
-        <h1>Quiz App soon</h1>
+        <div className="QuizButtons">
+            <div className="Btns">Head to Head</div>
+            <div className="Btns">Timed Quiz</div>
+            <div className="Btns">Survival Quiz</div>
+        </div>
+            <section>
+                <h4 style={{textAlign:"center"}}>Quiz Summary</h4>
+                <div>
+                    <h6>Recent Quizzes</h6>
+                    <table>
+                        <tr>1</tr>
+                        <tr>2</tr>
+                        <tr>3</tr>
+                    </table>
+
+                </div>
+                <span>30</span>
+            </section>
         </Wrapper>
     )
 }
