d6c827b50dadabdbdc3d39e2befa3697d611962d
b-lemy <lemabrian1234@gmail.com>
2023-03-08 13:02:46 +0300

    quiz app

--- frontend/src/Components/Forum/Forum.js
+++ frontend/src/Components/Forum/Forum.js
@@ -17,7 +17,7 @@ const Forum = () => {
             setPost(apiPosts.data.data)
         }
         getPosts();
-    }, [post])
+    }, [])
 
 
     return (
--- frontend/src/Components/Quiz/Quiz.js
+++ frontend/src/Components/Quiz/Quiz.js
@@ -7,41 +7,54 @@ const Quiz = () => {
     return (
         <Wrapper>
             <div className="QuizButtons">
-                <Link className="Btns">Head to Head</Link>
-                <Link className="Btns">Timed Quiz</Link>
-                <Link className="Btns">Survival Quiz</Link>
+                <Link to='' className="Btns">Head to Head</Link>
+                <Link to='' className="Btns">Timed Quiz</Link>
+                <Link to='' className="Btns">Survival Quiz</Link>
             </div>
-            <section>
+            <section className="QuizSection">
                 <h4 style={{textAlign: "center"}}>Quiz Summary</h4>
                 <h6>Recent Quizzes :</h6>
                 <div>
                     <table>
+                        <thead>
                         <tr>
                             <th>Quiz Type</th>
                             <th>Date</th>
                             <th>Score</th>
+                            <th>Attempts</th>
+
                         </tr>
+                        </thead>
+                        <tbody>
                         <tr>
                             <td>H2H</td>
                             <td>19/jan/2020</td>
                             <td>70 %</td>
+                            <td>7</td>
                         </tr>
+                        </tbody>
+                        <tbody>
                         <tr>
                             <td>Timed</td>
                             <td>5 days ago</td>
                             <td> 60 %</td>
+                            <td>2</td>
                         </tr>
+                        </tbody>
+                        <tbody>
                         <tr>
                             <td>H2H</td>
                             <td>3 hrs ago</td>
                             <td>40 %</td>
+                            <td>1</td>
                         </tr>
+                        </tbody>
                     </table>
 
                 </div>
                 <span className="spn">
-                    <p style={{fontSize: '12px',marginBottom:"0"}}>Average</p>
-                    <p style={{textAlign:"center",marginBottom:"0"}}> 56%</p>
+                    <p style={{fontSize: '12px', marginBottom: "0"}}>Average</p>
+                    <p style={{textAlign: "center", marginBottom: "0"}}> 56%</p>
                 </span>
             </section>
         </Wrapper>
