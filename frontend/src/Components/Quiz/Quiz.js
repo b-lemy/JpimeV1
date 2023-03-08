ee22d2c8050298ded6341d0b9fcb6151fa00f9fb
b-lemy <lemabrian1234@gmail.com>
2023-03-08 12:19:35 +0300

    quiz app

--- frontend/src/Components/Quiz/Quiz.js
+++ frontend/src/Components/Quiz/Quiz.js
@@ -1,27 +1,48 @@
 import React from "react";
 import Wrapper from "../Layout/Wrapper";
 import './Quiz.css'
+import {Link} from "react-router-dom";
 
 const Quiz = () => {
-    return(
+    return (
         <Wrapper>
-        <div className="QuizButtons">
-            <div className="Btns">Head to Head</div>
-            <div className="Btns">Timed Quiz</div>
-            <div className="Btns">Survival Quiz</div>
-        </div>
+            <div className="QuizButtons">
+                <Link className="Btns">Head to Head</Link>
+                <Link className="Btns">Timed Quiz</Link>
+                <Link className="Btns">Survival Quiz</Link>
+            </div>
             <section>
-                <h4 style={{textAlign:"center"}}>Quiz Summary</h4>
+                <h4 style={{textAlign: "center"}}>Quiz Summary</h4>
+                <h6>Recent Quizzes :</h6>
                 <div>
-                    <h6>Recent Quizzes</h6>
                     <table>
-                        <tr>1</tr>
-                        <tr>2</tr>
-                        <tr>3</tr>
+                        <tr>
+                            <th>Quiz Type</th>
+                            <th>Date</th>
+                            <th>Score</th>
+                        </tr>
+                        <tr>
+                            <td>H2H</td>
+                            <td>19/jan/2020</td>
+                            <td>70 %</td>
+                        </tr>
+                        <tr>
+                            <td>Timed</td>
+                            <td>5 days ago</td>
+                            <td> 60 %</td>
+                        </tr>
+                        <tr>
+                            <td>H2H</td>
+                            <td>3 hrs ago</td>
+                            <td>40 %</td>
+                        </tr>
                     </table>
 
                 </div>
-                <span>30</span>
+                <span className="spn">
+                    <p style={{fontSize: '12px',marginBottom:"0"}}>Average</p>
+                    <p style={{textAlign:"center",marginBottom:"0"}}> 56%</p>
+                </span>
             </section>
         </Wrapper>
     )
