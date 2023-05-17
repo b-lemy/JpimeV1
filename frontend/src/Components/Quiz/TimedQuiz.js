14b7acd9df7e1ca7a2b5c40a8f2676f9f4750264
b-lemy <lemabrian1234@gmail.com>
2023-05-17 11:28:56 +0300

    finished consuming the quiz api

--- frontend/src/Components/Quiz/TimedQuiz.js
+++ frontend/src/Components/Quiz/TimedQuiz.js
@@ -1,32 +1,41 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 import Wrapper from "../Layout/Wrapper";
 import './Quiz.css'
 import QuizApi from "./QuizApi";
-import quizApi from "./QuizApi";
 
 const TimedQuiz = () => {
+    const [quiz, setquiz] = useState([])
 
     useEffect(() => {
         const getQuiz = async () => {
             const apiPosts = await QuizApi.get("");
             console.log(apiPosts.data.results)
+            setquiz(apiPosts.data.results)
+
         }
         getQuiz();
     }, [])
     return (
         <Wrapper>
+
             <div className='timed-quiz'>
-                <div className='quiz_body'>
-                    <h2 className='quiz_question'> What is the capital city of Tanzania <br/>
-                    <span className="category">Geography</span></h2>
-                    <ol className="quiz-option">
-                        {/*<li className="selected">Arusha</li>*/}
-                        <li>Arusha</li>
-                        <li>Dodoma</li>
-                        <li>Dar es Salaam</li>
-                    </ol>
-                </div>
+                {quiz.map((item , index) => (
+                    <div key={index} className='quiz_body'>
+                        <h2 className='quiz_question'> {item.question} <br/>
+                            <span className="category">{item.category}</span></h2>
+                        <ol className="quiz-option">
+                            {/*<li className="selected">Arusha</li>*/}
+
+                            {item.incorrect_answers.map(answers =>(
+                                <li>{answers}</li>
+
+                            ))}
+                        </ol>
+                    </div>
+                ))
+                }
             </div>
+
         </Wrapper>
     )
 
