a2f57b4feb2cca9541fe24f5c1fc6da4bb7742b8
b-lemy <lemabrian1234@gmail.com>
2023-06-05 23:37:59 +0300

    landing page

--- frontend/src/Components/LandingPage/Landing.js
+++ frontend/src/Components/LandingPage/Landing.js
@@ -6,7 +6,38 @@ const Landing = () => {
         return (
             <div>
                 <Header/>
-                <h1>Landing page</h1>
+                <div className="landing">
+                    <div className="project">
+                        Project Name : Jipime Fasta Quiz App
+
+                        Description :A project where anybody could quiz himself up
+
+                        You have to Login to see the complete website
+
+                        sample email and password if you wont mind registering
+
+
+                        Feature Section
+                        Completed features its self quiz and forum site
+
+                        Quiz
+                        One will request for a quiz and then choose what category he or she
+                        wishes then quiz up
+
+                        Forum
+                        You can ask question and people will answer you if the wish though
+
+                        About Section
+                        What inspired me in the project
+
+                        LinkedIn :
+                        Github :
+
+                        GitHub Repo
+
+
+                    </div>
+                </div>
 
             </div>
         );
