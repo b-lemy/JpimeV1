1f916d9b57bcc330c8f7ce104ffbff56c128a9d3
b-lemy <lemabrian1234@gmail.com>
2023-06-06 22:50:14 +0300

    landing page

--- frontend/src/Components/LandingPage/Landing.js
+++ frontend/src/Components/LandingPage/Landing.js
@@ -8,19 +8,30 @@ const Landing = () => {
                 <Header/>
                 <div className="landing">
                     <div className="project">
-                       <p> Project Name : Jipime Fasta Quiz App
+                        <p> Project Name :<span style={{fontWeight: "bolder"}}> Jipime Fasta Quiz App </span>
                            <br/>
 
 
                         Description :A project where anybody could quiz himself up
                            <br/>
+                           <br/>
                         You have to Login to see the complete website
 
                         sample email and password if you wont mind registering
+                           <br/>
+                           <br/>
+                           <span  style={{fontWeight: "bolder"}}>
+                               <span>Email:  orogahn@gmail.com</span>
+                               <br/>
+                               <span>Password: password </span>
+                               </span>
 
+
+                           <br/>
                            <br/>
                         Feature Section
                         Completed features its self quiz and forum site
+                           <br/>
                            <br/>
                         Quiz
                         One will request for a quiz and then choose what category he or she
@@ -32,10 +43,14 @@ const Landing = () => {
                         About Section
                         What inspired me in the project
                            <br/>
-                        LinkedIn :
-                        Github :
                            <br/>
-                        GitHub Repo
+                           <a style={{color:"darkgray" }} href="https://www.linkedin.com/in/brian-lema-3b731920b/" target='_blank'>Visit my LinkedIn profile</a>
+                           <br/>
+                           <br/>
+                           <a style={{color:"darkgray" }} href="https://github.com/b-lemy" target='_blank'>Visit my Github Account</a>
+                           <br/>
+                           <br/>
+                        <a style={{color:"darkgray" }} href="https://github.com/b-lemy/JpimeV1" target='_blank'>Visit the Github Repo</a>
                        </p>
 
                     </div>
