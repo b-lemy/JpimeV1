f183506e9688ec0ba182543b5479156dac4dcf3c
b-lemy <lemabrian1234@gmail.com>
2023-06-06 21:42:19 +0300

    landing page

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -17,10 +17,10 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-// Route::get('/trial',function (){
-//     return ('Mkombozi123');
-////     return Hash::make('Mkombozi123');
-// });
+ Route::get('/trial',function (){
+     return ('Mkombozi123');
+//     return Hash::make('Mkombozi123');
+ });
 
 Route::post('login',[AuthController::class,'login']);
 Route::post('register',[AuthController::class,'register']);
--- frontend/src/Components/LandingPage/Landing.js
+++ frontend/src/Components/LandingPage/Landing.js
@@ -8,33 +8,35 @@ const Landing = () => {
                 <Header/>
                 <div className="landing">
                     <div className="project">
-                        Project Name : Jipime Fasta Quiz App
+                       <p> Project Name : Jipime Fasta Quiz App
+                           <br/>
+
 
                         Description :A project where anybody could quiz himself up
-
+                           <br/>
                         You have to Login to see the complete website
 
                         sample email and password if you wont mind registering
 
-
+                           <br/>
                         Feature Section
                         Completed features its self quiz and forum site
-
+                           <br/>
                         Quiz
                         One will request for a quiz and then choose what category he or she
                         wishes then quiz up
-
+                           <br/>
                         Forum
                         You can ask question and people will answer you if the wish though
-
+                           <br/>
                         About Section
                         What inspired me in the project
-
+                           <br/>
                         LinkedIn :
                         Github :
-
+                           <br/>
                         GitHub Repo
-
+                       </p>
 
                     </div>
                 </div>
