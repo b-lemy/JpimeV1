1add0bfe83d9db89f103170d0a98a162c8dcbf31
b-lemy <brian.lema@gmail.com>
2025-04-04 19:59:40 +0300

    login configs

--- backend/database/factories/UserFactory.php
+++ backend/database/factories/UserFactory.php
@@ -35,6 +35,7 @@ class UserFactory extends Factory
             'avatar'=> $randomImages[rand(0, 10)],
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+             # 'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
 
         ];
